b3d303340f4b17c6821f5a86e86cc832316ba6b8
EvgenyGr <140063448+Demorein@users.noreply.github.com>
2024-05-21 18:55:00 +0300

    v2.0 $0.3

--- error_codes.py
+++ error_codes.py
@@ -1,3 +1,2 @@
-e41 = "ERROR 41\ne41 - The token input field is not filled in or filled in incorrectly\n\nTo avoid critical errors, the variable token = 0$e41"
-e42 = "ERROR 42\ne42 - The userid input field is not filled in or filled in incorrectly\n\nTo avoid critical errors, the variable userid = 0$e42"
+e41 = "ERROR 41\ne41 - The token or user id input field is not filled in or filled in incorrectly\n\nTo avoid critical errors, the variable token = 0 and userid = 0$e41"
 e0 = "ERROR 0\ne0 - Unknown error$e0"
--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from wins import *
 def main():
     root = tk.Tk()
     root.resizable(False, False)
-    root.geometry("400x200")
+    root.geometry("400x130")
     root.title("RuFA SB Monitor")
+    
 
     btnOpenConfig = ttk.Button(root, text="Open Config", command=WinConfig, width=13).place(x=310,y=10)
     btnOpenSett = ttk.Button(root, text= "Open Settings", command=WinSett, width=13).place(x=310,y=40)
-    btnOpenAbout = ttk.Button(root, text= "About RuFA SB", command=WinAbout, width=13).place(x=310,y=170)
+    btnOpenAbout = ttk.Button(root, text= "About RuFA SB", command=WinAbout, width=13).place(x=310,y=100)
+    InfoLabel = ttk.Label(text="RuFA Shutdown Bot Monitor").place(x=0,y=100)
 
     root.mainloop()
 
 if __name__ == "__main__":
+    th.Thread(target = update).start()
     main()
 
 
